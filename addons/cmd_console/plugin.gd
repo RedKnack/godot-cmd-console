28617a79d614fb425853b680834ed9b6339a765a
RedKnack Interactive <social@redknack.com>
2026-01-31 17:22:36 +0100

    Update plugin.gd for Godot 4.6 compatibility

--- addons/cmd_console/plugin.gd
+++ addons/cmd_console/plugin.gd
@@ -1,14 +1,21 @@
+# plugin.gd
 @tool
 extends EditorPlugin
 
-var panel: Control
+var console_panel: Control
 
-func _enter_tree() -> void:
-    panel = preload("res://addons/cmd_console/console_panel.tscn").instantiate()
-    add_control_to_bottom_panel(panel, "CMD")
+func _enter_tree():
+    console_panel = preload("res://addons/cmd_console/console_panel.tscn").instantiate()
+    
+    if console_panel.custom_minimum_size == Vector2.ZERO:
+        console_panel.custom_minimum_size = Vector2(300, 150)
+    
+    add_control_to_bottom_panel(console_panel, "Console")
+    
+    print("[Console] Successfully added to bottom panel")
 
-func _exit_tree() -> void:
-    if panel:
-        remove_control_from_bottom_panel(panel)
-        panel.queue_free()
-        panel = null
+func _exit_tree():
+    if console_panel:
+        remove_control_from_bottom_panel(console_panel)
+        console_panel.queue_free()
+        console_panel = null
